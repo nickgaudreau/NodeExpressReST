de31e891be5d82a71036fc1eb2f48d39f9ad06f5
nickgoodrow <nickgoodrow@gmail.com>
2017-02-11 21:55:02 -0500

    Mongoose => mongoDB => BookApi db Coll books Added GET and GETByID route Added book model

--- app.js
+++ app.js
@@ -1,20 +1,65 @@
-var express = require('express');
+var express = require('express'); // import express object reference 
+var mongoose = require('mongoose');
 
-// start express
+// open connection to DB, passing in mongodb conn string => like a dbContext
+var db = mongoose.connect('mongodb://localhost:12345/bookApi');
+
+// this return our book model hook up to mongoose that gets form mongoDb
+var Book = require('./models/bookModel');
+
+//  express instance
 var app = express();
 
 // setup a port
 var port = process.env.PORT || 3333; // if this does not return : process.env.PORT return 3333
 
+var bookRouter = express.Router(); // router instance
+
+// Get all
+bookRouter.route('/Books')
+    .get(function(req, res){
+        // query string ... like OData
+        var query = req.query;
+        Book.find(query, function(err, books){
+            if(err){
+                console.error(err);
+                res.status(500).send(err); // send 500 and details to cleint
+            }
+            else{
+                console.log('success');
+                res.json(books);
+            }
+        });
+    });
+
+// Get by ID
+bookRouter.route('/Books/:bookId')
+    .get(function(req, res){
+        // params bookId must match match the route string above e.g. '/Books/:bookId'
+        Book.findById(req.params.bookId ,function(err, book){
+            if(err){
+                console.error(err);
+                res.status(500).send(err); // send 500 and details to cleint
+            }
+            else{
+                console.log('success');
+                res.json(book);
+            }
+        });
+    });
+
+app.use('/api', bookRouter);
+
 // setup a handler for a route, when it hits the root
 // param root, callback()
 app.get('/', function(req, res){
-    res.send('Hi from express ReST web api')
+    // send => string of text
+    res.send('Hi from express ReST web api using node and gulp')
 })
 
 // Losten for connections: must listen to port and we''ll simply log on success listen to port
 app.listen(port, function(){
-    console.log('Running on PORT:' + port);
+    console.log('Gulp running app on PORT:' + port);
 });
 
 // TEst by typing node  app.js in shell/termnal/cmd/PS
---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp'),
+    nodemon = require('nodemon');
+
+// create a default task to run nodemon pluggin
+gulp.task('default', function(){
+    nodemon({
+        script: 'app.js',
+        ext: 'js',
+        env: {
+            PORT: 8008
+        },
+        ignore:['./node_modules/**']
+    })
+    .on('restart', function(){
+        console.log('Restarting')
+    })
+    ;
+});
